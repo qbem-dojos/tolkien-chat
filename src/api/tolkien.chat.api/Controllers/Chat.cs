ae537f8482d7d953f2b8ee411e61b5f7733a9ee0
brunovicenteb <brunovicenteb@gmail.com>
2022-06-18 14:34:40 -0300

    Criação do projeto partindo de um template de API gerado pelo Visual Studio

---
+++ src/api/tolkien.chat.api/Controllers/Chat.cs
@@ -0,0 +1,7 @@
+namespace tolkien.chat.api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Chat : ControllerBase
+{
+}
---
+++ src/api/tolkien.chat.api/GlobalUsing.cs
@@ -0,0 +1,6 @@
+global using System;
+global using System.Linq;
+global using Microsoft.AspNetCore.Builder;
+global using System.Threading.Tasks;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Collections.Generic;
